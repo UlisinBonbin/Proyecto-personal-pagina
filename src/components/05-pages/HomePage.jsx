0ca5a3a4e1aaac7b90eb461d09d06a330536f703
UlisinBonbin <74909200+UlisinBonbin@users.noreply.github.com>
2025-12-14 15:22:27 -0300

    HOla

--- src/components/05-pages/HomePage.jsx
+++ src/components/05-pages/HomePage.jsx
@@ -8,6 +8,7 @@ export default function HomePage() {
             <h1>Bienvenido a la Página Principal</h1>
             <p>Esta es la página de inicio de nuestro sitio web.</p>
             <p>Aquí puedes encontrar información relevante y navegar a otras secciones.</p>
+            <p>¡Explora y disfruta de tu visita!</p>
         </section>
     </MainLayout>
     
