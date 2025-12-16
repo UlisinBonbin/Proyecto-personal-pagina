ee41cd1ebcf6365f70909b862e72f8f18fc8a0a5
UlisinBonbin <74909200+UlisinBonbin@users.noreply.github.com>
2025-12-15 23:12:19 -0300

    Falta crear un css para los parrafos y demas, para cada pagina, la idea es que vaya en organismo, tambien ver si ordenar en una carpeta el Header, y cambiar el componetne jsx a otra parte

--- src/components/05-pages/HomePage.jsx
+++ src/components/05-pages/HomePage.jsx
@@ -6,9 +6,6 @@ export default function HomePage() {
     <MainLayout>
         <section>
             <h1>Bienvenido a Peluchitos Bonbin</h1>
-            <p>Esta es la página de inicio de nuestro sitio web.</p>
-            <p>Aquí puedes encontrar información relevante y navegar a otras secciones.</p>
-            <p>¡Explora y disfruta de tu visita!</p>
         </section>
     </MainLayout>
     
