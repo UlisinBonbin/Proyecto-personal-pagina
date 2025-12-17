9be33a658f154d6b38b77dc315d95eb476fd604a
UlisinBonbin <74909200+UlisinBonbin@users.noreply.github.com>
2025-12-17 10:42:27 -0300

    Anadi imaganes y englove las cosas en un div

--- src/components/05-pages/Home/HomePage.jsx
+++ src/components/05-pages/Home/HomePage.jsx
@@ -6,8 +6,24 @@ export default function HomePage() {
   return (
     <MainLayout>
       <section className="home-hero">
+        <div className="hero-content">
         <h1>Bienvenido a Peluchitos Bonbin</h1>
+          <p>
+            Peluchitos Bonbin es una tienda que acaba de surgir, vendemos los mejores
+            peluches relacionados al mundo del anime, videojuegos y series.
+          </p>
+
+        <h2>Nuestros peluchitos más vendidos C:</h2>
+
+          <div className="peluches-destacados">
+            <img src="https://m.media-amazon.com/images/I/51hleVmiOEL._AC_SL1000_.jpg" alt="Peluche de Ponmy" />
+            <img src="https://www.kawaiibarcelona.com/wp-content/uploads/2018/09/peluchetotoropeque.png" alt="Peluche de Totoro" />
+            <img src="https://m.media-amazon.com/images/I/51kQbkIQFjL._AC_SL1000_.jpg" alt="Peluche de Ralsei" />
+          </div>
+        </div>
       </section>
+
+
     </MainLayout>
   )
 }
