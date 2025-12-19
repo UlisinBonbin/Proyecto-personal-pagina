57126c88887ffa088c971dc8a7d9c934553ad9be
UlisinBonbin <74909200+UlisinBonbin@users.noreply.github.com>
2025-12-19 18:18:02 -0300

    Se agrego el logo al header

--- src/components/03-organisms/Header.jsx
+++ src/components/03-organisms/Header.jsx
@@ -1,18 +1,21 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import Button from '../01-atoms/button/Button';
-import logoImg from '../../assets/images/logos/logo_pagina.png'
 import './Header.css';
+import logoImg from '../../assets/images//logos/logo_pagina.png';
 
 
 export default function Header() {
   return (
     <header className="header-container">
+
       <div className="header-logo">
         <Link to="/">
-          <img src={logoImg} alt="Logo de la tienda de peluches" />
+          <img src={logoImg} alt="Peluches y Amor" />
         </Link>
       </div>
+
+
       <nav className="header-nav">
         <Link to="/">
           <Button variant="secondary">Inicio</Button>
@@ -30,8 +33,6 @@ export default function Header() {
           <Button variant="secondary">Contacto</Button>
         </Link>
       </nav>
-
-      <div /> {/* columna vacía para balancear */}
     </header>
   );
 }
