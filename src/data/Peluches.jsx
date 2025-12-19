852a0eef807e3c862aa9e032f1cf97187923d07d
UlisinBonbin <74909200+UlisinBonbin@users.noreply.github.com>
2025-12-19 13:24:02 -0300

    se cambio las imagenes, ya no esan urls si no que son locales, solo el cambio peluches, de data y el ananido de las imagenes en asstes, images

--- src/data/Peluches.jsx
+++ src/data/Peluches.jsx
@@ -1,29 +1,53 @@
-const peluches = [
-  { id: 1, nombre: 'Ponmy', precio: '$12.990', imagen: 'https://m.media-amazon.com/images/I/51hleVmiOEL._AC_SL1000_.jpg', category: 'internet' },
-  { id: 2, nombre: 'Totoro', precio: '$18.990', imagen: 'https://www.kawaiibarcelona.com/wp-content/uploads/2018/09/peluchetotoropeque.png', category: 'anime' },
-  { id: 3, nombre: 'Ralsei', precio: '$15.990', imagen: 'https://m.media-amazon.com/images/I/51kQbkIQFjL._AC_SL1000_.jpg', category: 'videojuegos' },
-  { id: 4, nombre: 'Luffy', precio: '$22.990', imagen: 'https://www.pro-bems.com/IMAGES/images_1/FIG160019472454/m/FIG160019472454_1.png', category: 'anime' },
-  { id: 5, nombre: 'Zoro', precio: '$19.990', imagen: 'https://images-cdn.ubuy.co.za/63abfb2b413e5f1e034336d3-one-piece-plush-doll-yurutto-one-piece.jpg', category: 'anime' },
-  { id: 6, nombre: 'Den Den Mushi Law', precio: '$14.990', imagen: 'https://meccha-japan.com/583307-large_default/peluche-den-den-mushi-trafalgar-law-one-piece.jpg', category: 'anime' },
-  { id: 7, nombre: 'Chopper', precio: '$16.990', imagen: 'https://www.mi-peluche.com/wp-content/uploads/sites/4/2021/10/peluche-mono-chopper-de-one-piece.png', category: 'anime' },
-  { id: 8, nombre: 'Winton Overwat', precio: '$13.990', imagen: 'https://m.media-amazon.com/images/I/416gr5R0fdL._AC_UF894,1000_QL80_.jpg', category: 'videojuegos' },
-  { id: 9, nombre: 'Mario', precio: '$17.990', imagen: 'https://gcjuegos.cl/1068-large_default/peluche-mario-bros-plush-14.jpg', category: 'videojuegos' },
-  { id: 10, nombre: 'Luigi', precio: '$17.990', imagen: 'https://m.media-amazon.com/images/I/61V9zKFuZpL._AC_SL1361_.jpg', category: 'videojuegos' },
-  { id: 11, nombre: 'Peach felina', precio: '$17.990', imagen: 'https://i.pinimg.com/736x/c5/c3/28/c5c3285e342453e4ce7da908178e3e96.jpg', category: 'videojuegos' },
-  { id: 12, nombre: 'Cloud Strife', precio: '$17.990', imagen: 'https://m.media-amazon.com/images/I/61WZGOIVW9L.jpg', category: 'videojuegos' },
-  { id: 13, nombre: 'Sephirot', precio: '$17.990', imagen: 'https://m.media-amazon.com/images/I/61mYbM4wCIL._AC_UF894,1000_QL80_.jpg', category: 'videojuegos' },
-  { id: 14, nombre: 'Aerith', precio: '$17.990', imagen: 'https://m.media-amazon.com/images/I/61T-Y1G61hL._AC_UF894,1000_QL80_.jpg', category: 'videojuegos' },
-  { id: 15, nombre: 'Tifa', precio: '$17.990', imagen: 'https://m.media-amazon.com/images/I/51GQsYFGO+L._AC_UF894,1000_QL80_.jpg', category: 'videojuegos' },
-  { id: 16, nombre: 'Sonic', precio: '$17.990', imagen: 'https://m.media-amazon.com/images/I/51rPCDwoSSL._AC_SL1001_.jpg', category: 'videojuegos' },
-  { id: 17, nombre: 'Tails drogado', precio: '$17.990', imagen: 'https://dcdn-us.mitiendanube.com/stores/001/889/282/products/diseno-sin-titulo-301-0eac151009c651438016519452214431-1024-1024.png', category: 'videojuegos' },
-  { id: 18, nombre: 'Shadow', precio: '$17.990', imagen: 'https://m.media-amazon.com/images/I/61O-w+mLyNL._AC_UF894,1000_QL80_.jpg', category: 'videojuegos' },
-  { id: 19, nombre: 'King', precio: '$17.990', imagen: 'https://m.media-amazon.com/images/I/61dd+zHBfGL._AC_UF894,1000_QL80_.jpg', category: 'internet' },
-  { id: 20, nombre: 'Caine', precio: '$17.990', imagen: 'https://m.media-amazon.com/images/I/61VaFwZPbFL._AC_UF894,1000_QL80_.jpg', category: 'internet' },
-  { id: 21, nombre: 'Jax', precio: '$17.990', imagen: 'https://m.media-amazon.com/images/I/71jKFm8B8NL._AC_UF350,350_QL80_.jpg', category: 'internet' },
-  { id: 22, nombre: 'Whis', precio: '$17.990', imagen: 'https://pelucheuniverse.com/2061-large_default/peluche-dragon-ball-super-whis.jpg', category: 'anime' },
-  { id: 23, nombre: 'Goku', precio: '$17.990', imagen: 'https://m.media-amazon.com/images/I/613A1hPRGuL._AC_UF894,1000_QL80_.jpg', category: 'anime' },
-  { id: 24, nombre: 'Bills', precio: '$17.990', imagen: 'https://m.media-amazon.com/images/I/71j-fkghp3L.jpg', category: 'anime' }
+import ponmyImg from '../assets/images/peluches/ponmy.jpg'
+import totoroImg from '../assets/images/peluches/totoro.png'
+import ralseiImg from '../assets/images/peluches/ralsei.jpg'
+import luffyImg from '../assets/images/peluches/luffy.png'
+import zoroImg from '../assets/images/peluches/zoro.png'
+import lawImg from '../assets/images/peluches/den-den-mushi-law.jpg'
+import chopperImg from '../assets/images/peluches/mono-chopper.png'
+import winstonImg from '../assets/images/peluches/winton.jpg'
+import marioImg from '../assets/images/peluches/mario.jpg'
+import luigiImg from '../assets/images/peluches/luigi.jpg'
+import peachImg from '../assets/images/peluches/peach-felina.jpg'
+import cloudImg from '../assets/images/peluches/cloud.jpg'
+import sephirotImg from '../assets/images/peluches/sephirot.jpg'
+import aerithImg from '../assets/images/peluches/aerith.jpg'
+import tifaImg from '../assets/images/peluches/tifa.jpg'
+import sonicImg from '../assets/images/peluches/sonic.jpg'
+import tailsImg from '../assets/images/peluches/tails-drogado.png'
+import shadowImg from '../assets/images/peluches/shadow.jpg'
+import kingImg from '../assets/images/peluches/king.jpg'
+import caineImg from '../assets/images/peluches/caine.jpg'
+import jaxImg from '../assets/images/peluches/jax.jpg'
+import whisImg from '../assets/images/peluches/whis.jpg'
+import gokuImg from '../assets/images/peluches/goku.jpg'
+import billsImg from '../assets/images/peluches/bills.jpg'
 
+const peluches = [
+  { id: 1, nombre: 'Ponmy', precio: '$12.990', imagen: ponmyImg, category: 'internet' },
+  { id: 2, nombre: 'Totoro', precio: '$18.990', imagen: totoroImg, category: 'anime' },
+  { id: 3, nombre: 'Ralsei', precio: '$15.990', imagen: ralseiImg, category: 'videojuegos' },
+  { id: 4, nombre: 'Luffy', precio: '$22.990', imagen: luffyImg, category: 'anime' },
+  { id: 5, nombre: 'Zoro', precio: '$19.990', imagen: zoroImg, category: 'anime' },
+  { id: 6, nombre: 'Den Den Mushi Law', precio: '$14.990', imagen: lawImg, category: 'anime' },
+  { id: 7, nombre: 'Chopper', precio: '$16.990', imagen: chopperImg, category: 'anime' },
+  { id: 8, nombre: 'Winton Overwat', precio: '$13.990', imagen: winstonImg, category: 'videojuegos' },
+  { id: 9, nombre: 'Mario', precio: '$17.990', imagen: marioImg, category: 'videojuegos' },
+  { id: 10, nombre: 'Luigi', precio: '$17.990', imagen: luigiImg, category: 'videojuegos' },
+  { id: 11, nombre: 'Peach felina', precio: '$17.990', imagen: peachImg, category: 'videojuegos' },
+  { id: 12, nombre: 'Cloud Strife', precio: '$17.990', imagen: cloudImg, category: 'videojuegos' },
+  { id: 13, nombre: 'Sephirot', precio: '$17.990', imagen: sephirotImg, category: 'videojuegos' },
+  { id: 14, nombre: 'Aerith', precio: '$17.990', imagen: aerithImg, category: 'videojuegos' },
+  { id: 15, nombre: 'Tifa', precio: '$17.990', imagen: tifaImg, category: 'videojuegos' },
+  { id: 16, nombre: 'Sonic', precio: '$17.990', imagen: sonicImg, category: 'videojuegos' },
+  { id: 17, nombre: 'Tails drogado', precio: '$17.990', imagen: tailsImg, category: 'videojuegos' },
+  { id: 18, nombre: 'Shadow', precio: '$17.990', imagen: shadowImg, category: 'videojuegos' },
+  { id: 19, nombre: 'King', precio: '$17.990', imagen: kingImg, category: 'internet' },
+  { id: 20, nombre: 'Caine', precio: '$17.990', imagen: caineImg, category: 'internet' },
+  { id: 21, nombre: 'Jax', precio: '$17.990', imagen: jaxImg, category: 'internet' },
+  { id: 22, nombre: 'Whis', precio: '$17.990', imagen: whisImg, category: 'anime' },
+  { id: 23, nombre: 'Goku', precio: '$17.990', imagen: gokuImg, category: 'anime' },
+  { id: 24, nombre: 'Bills', precio: '$17.990', imagen: billsImg, category: 'anime' }
 ]
 
 export default peluches
